76ebf583fb690cbd2e77f99362ce33a7ed6b8fd1
Miłosz <devmilek@gmail.com>
2023-12-18 02:28:11 +0100

    Add DashboardPage and Navbar components

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const DashboardPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+
+  return <div>DashboardPage</div>;
+};
+
+export default DashboardPage;
